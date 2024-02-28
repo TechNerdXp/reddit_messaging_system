a49d55c58fc981bf0f26a86edae0a46449c402fd
Muhammad Nadeem <technerdxp@gmail.com>
2024-02-28 17:08:21 +0500

    csv dump function added to test.py

--- test.py
+++ test.py
@@ -64,7 +64,14 @@ def reply_to_message_by_id(reddit, message_id):
 
     
 # print(send_message(reddit, 'NadeemGorsi', 'subject', 'test message body2'))def send_message(reddit, username, subject, body)
-print(get_messages(reddit))
+# print(get_messages(reddit))
 
+def dump_to_csv(data, filename='temp.csv'):
+    keys = data[0].keys()
+
+    with open(filename, 'w', newline='', encoding='utf-8') as output_file:
+        dict_writer = csv.DictWriter(output_file, keys)
+        dict_writer.writeheader()
+        dict_writer.writerows(data)
 
     
