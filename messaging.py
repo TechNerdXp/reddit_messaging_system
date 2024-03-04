b22fa13e8f393cc82e533b07bb55d96d73823f3a
Muhammad Nadeem <technerdxp@gmail.com>
2024-03-04 05:27:16 +0500

    messaging module update for 3 message statuses

--- messaging.py
+++ messaging.py
@@ -1,37 +1,28 @@
 from reddit import send_message, get_messages
-from ai import create_thread, add_message, get_thread_messages
+from ai import create_thread, add_message, get_thread_messages, run_assistant
 from project_db import get_posts, insert_message, check_message_status, update_message_status, update_openai_thread_id, update_reddit_message_id, mark_message_replied
-
 import time
 
 # Get all posts
 posts = get_posts()
 
 for post in posts:
-    print(post['openai_thread_id'])
-    continue
-    # If the post doesn't have an OpenAI thread ID, create a new thread
-    if not post['openai_thread_id']:
+    message_status = post['message_status']
+    post_id = post['id']
+    if message_status == 'thread_not_started':
+        message = post['title'] + ' ' + post['text']
         thread_id = create_thread()
+        add_message(message, thread_id)
+        # insert_message(post_id, message, message_id, source)
         update_openai_thread_id(post['id'], thread_id)
-        add_message(post['title'] + ' ' + post['text'], thread_id)
-    else:
-        thread_id = post['openai_thread_id']
+        run_assistant(thread_id)
+        update_message_status(post['id'], 'waiting_for_the_assistant')
+    elif message_status == 'waiting_for_the_assistant':
+        
+        pass
+    elif message_status == 'waiting_for_the_user':
+        pass
 
-    # Get all messages in the thread
-    thread_messages = get_thread_messages(thread_id)
-
-    # If the last message is from the assistant and the message status is 'waiting_for_user'
-    if thread_messages[-1]['source'] == 'openai' and post['message_status'] == 'waiting_for_user':
-        # Send the assistant's message to the user
-        send_message(post['author'], 'Re: ' + post['title'], thread_messages[-1]['message'])
-        update_message_status(post['id'], 'waiting_for_assistant')
-
-    # If the last message is from the user and the message status is 'waiting_for_assistant'
-    elif thread_messages[-1]['source'] == 'reddit' and post['message_status'] == 'waiting_for_assistant':
-        # Add the user's message to the thread
-        add_message(thread_messages[-1]['message'], thread_id)
-        update_message_status(post['id'], 'waiting_for_user')
-
-    # Wait for a while before the next iteration
-    time.sleep(5)
+    
+    
+time.sleep(5)
