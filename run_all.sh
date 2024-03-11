9322393b44d31142a5c27c9391acf21d5e05d378
Muhammad Nadeem <technerdxp@gmail.com>
2024-03-11 22:43:43 +0500

    ngrock command in run_all domain fix

--- run_all.sh
+++ run_all.sh
@@ -14,7 +14,7 @@ fi
 screen -dmS server bash -c 'flask run --port 5010; exec sh'
 
 # Optional Ngrock Step
-screen -dmS ngrock bash -c 'ngrok http --domain=https://reddiappinterface.ngrok-free.app 5010; exec sh'
+screen -dmS ngrock bash -c 'ngrok http --domain=reddiappinterface.ngrok-free.app 5010; exec sh'
 
 # Running Messaging Module
 # We use sleep to delay the execution of the messaging module
